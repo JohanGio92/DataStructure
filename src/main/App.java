712f67aa9d0130410212a58bf1b58231f90f1964
Johan Revilla <johan_revilla_92@hotmail.com>
2022-09-26 16:46:02 -0300

    finish avl tree

--- src/main/App.java
+++ src/main/App.java
@@ -14,13 +14,31 @@ public class App {
 		tree.add(50);
 		tree.add(15);
 		tree.add(10);
+		tree.add(10);
+		tree.add(10);
 		
-		for (int i = 0; i < 1000; i++) {
-			tree.add(random.nextInt(1000));
+		for (int i = 0; i < 200; i++) {
+			tree.add(random.nextInt(100));
 		}
+		
+		System.out.println(tree.size());
+		for (int i = 0; i < 500; i++) {
+			int randomNumber = random.nextInt(100);
+			if (tree.has(randomNumber)) {
+				tree.remove(randomNumber);
+			}
+		}
+		System.out.println("level");
+		tree.levelOrder();
+		System.out.println("=================");
+		System.out.println("size: " + tree.size());
+		System.out.println("=================");
+		System.out.println("inOrder");
 		tree.inOrder();
 		System.out.println("=================");
+		System.out.println("preOrder");
 		tree.preOrder();
+		System.out.println(tree.has(5));
 	}
 
 }
--- src/tree/AVLTree.java
+++ src/tree/AVLTree.java
@@ -1,7 +1,12 @@
 package tree;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class AVLTree<E extends Comparable<E>> {
 	
+	private static final int BALANCE = 1;
 	private Node<E> root;
 	private int size;
 	
@@ -10,13 +15,70 @@ public class AVLTree<E extends Comparable<E>> {
 		root = null;
 	}
 	
+	public boolean has(E element) {
+		return hasNode(element) != null;
+	}
+	
+	private Node<E> hasNode(E element) {
+		return hasNode(root, element);
+	}
+	
+	private Node<E> hasNode(Node<E> parent, E element) {
+		if(parent == null) {
+			return null;
+		}
+		
+		if(element.compareTo(parent.getElement()) > 0) {
+			parent = hasNode(parent.rightChild(), element);
+		}
+		else if(parent.getElement().compareTo(element) > 0) {
+			parent = hasNode(parent.leftChild(), element);
+		}
+		
+		return parent;
+	}
+	
+	public void remove(E element) {
+		assert has(element);
+		root = remove(root, element);
+		--size;
+	}
+
+	private Node<E> remove(Node<E> parent, E element) {
+		
+		if(element.compareTo(parent.getElement()) > 0) {
+			parent.rightChild(remove(parent.rightChild(), element));
+		}
+		else if(parent.getElement().compareTo(element) > 0) {
+			parent.leftChild(remove(parent.leftChild(), element));
+		}
+		else {
+			if(parent.isLeftEmpty() && parent.isRightEmpty()) {
+				parent = null;
+			} else if (parent.isLeftEmpty()) {
+				parent = parent.rightChild();
+			} else if (parent.isRightEmpty()) {
+				parent = parent.leftChild();
+			} else {
+				Node<E> minNode = minNode(parent.rightChild());
+				parent.setElement(minNode.getElement());
+				parent.rightChild(remove(parent.rightChild(), minNode.getElement()));
+			}
+		}
+		
+		return parent == null ? parent : checkBalance(parent);
+	}
+	
+	private Node<E> minNode(Node<E> iterator) {
+		while (!iterator.isLeftEmpty()) {
+			iterator = iterator.leftChild();
+		}
+		return iterator;
+	}
+
 	public void add(E element) {
 		Node<E> newChild = new Node<E>(element);
-		if(root != null) {
-			root = add(root, newChild);
-		} else {
-			root = newChild;
-		}
+		root = root != null ? add(root, newChild) : newChild;
 		size++;
 	}
 	
@@ -38,34 +100,34 @@ public class AVLTree<E extends Comparable<E>> {
 		return checkBalance(parent);
 	}
 
-	private Node<E> checkBalance(Node<E> newChild) {
-		if(!hasHeightBalance(newChild)) {
-			newChild = rebalance(newChild);
+	private Node<E> checkBalance(Node<E> parent) {
+		if(!hasHeightBalance(parent)) {
+			parent = rebalance(parent);
 		}
-		return newChild;
+		return parent;
 	}
 
-	private Node<E> rebalance(Node<E> newChild) {
-		if(height(newChild.leftChild()) - height(newChild.rightChild()) > 1) {
-			if(height(newChild.leftChild().leftChild()) > height(newChild.leftChild().rightChild())) {
-				newChild = rightRotation(newChild);
+	private Node<E> rebalance(Node<E> grandParent) {
+		if(height(grandParent.leftChild()) - height(grandParent.rightChild()) > BALANCE) {
+			if(height(grandParent.leftChild().leftChild()) > height(grandParent.leftChild().rightChild())) {
+				grandParent = rightRotation(grandParent);
 			}
 			else {
-				newChild = leftRightRotation(newChild);
+				grandParent = leftRightRotation(grandParent);
 			}
 		} else {
-			if(height(newChild.rightChild().rightChild()) > height(newChild.rightChild().leftChild())) {
-				newChild = leftRotation(newChild);
+			if(height(grandParent.rightChild().rightChild()) > height(grandParent.rightChild().leftChild())) {
+				grandParent = leftRotation(grandParent);
 			}
 			else {
-				newChild = rightLeftRotation(newChild);
+				grandParent = rightLeftRotation(grandParent);
 			}
 		}
-		return newChild;
+		return grandParent;
 	}
 
-	private boolean hasHeightBalance(Node<E> newChild) {
-		return Math.abs(height(newChild.leftChild()) - height(newChild.rightChild())) <= 1;
+	private boolean hasHeightBalance(Node<E> parent) {
+		return Math.abs(height(parent.leftChild()) - height(parent.rightChild())) <= BALANCE;
 	}
 	
 	private int height(Node<E> parent) {
@@ -104,5 +166,26 @@ public class AVLTree<E extends Comparable<E>> {
 
 	public void preOrder() {
 		root.preOrder();
+	}
+	
+	public void levelOrder() {
+		levelOrder(root);
+	}
+	
+	private void levelOrder(Node<E> parent) {
+		Queue<Node<E>> queue = new LinkedList<>();
+		queue.add(parent);
+		while(!queue.isEmpty()){
+			Node<E> current = queue.remove();
+			System.out.println(current.getElement());
+			if(!current.isLeftEmpty())
+				queue.add(current.leftChild());
+			if(!current.isRightEmpty())
+				queue.add(current.rightChild());
+		}
+	}
+
+	public int size() {
+		return size;
 	}
 }
